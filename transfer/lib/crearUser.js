55c13826827bf52c1d2a48db32eae3180c434d63
Dmytro Zilnyk <zdima@MacBook-Pro-de-Dmytro.local>
2018-02-18 21:56:16 +0100

    Login y Registro

--- transfer/lib/crearUser.js
+++ transfer/lib/crearUser.js
@@ -5,42 +5,62 @@
  */
 
 function createUser(user) {
-      return getParticipantRegistry('org.transfer.tfg.User')
-            .then(function (participantRegistry) {;
-        		  var factory = getFactory();
-                  var newUser = factory.newResource('org.transfer.tfg', 'User', user.email);
-                  newUser.name = user.name;
-                  newUser.lastName = user.lastName;
-                  newUser.email = user.email;
-                  newUser.date = user.date;
-                  newUser.country = user.country;
-                  newUser.phoneNumber = user.phoneNumber;
-                  newUser.password = user.password;
-                  return participantRegistry.add(newUser);
-             })
-            .catch(function (error) {
-                  throw new Error("No se ha podido crear el usuario");
-            });
+  return getParticipantRegistry('org.transfer.tfg.User')
+    .then(function (participantRegistry) {
+      return participantRegistry.exists(user.email);
+    })
+    .then(function (exists) {
+      if (!exists) {
+        return getParticipantRegistry('org.transfer.tfg.User')
+          .then(function (participantRegistry) {
+            var newUser = getFactory().newResource('org.transfer.tfg', 'User', user.email);
+            newUser.name = user.name;
+            newUser.lastName = user.lastName;
+            newUser.email = user.email;
+            newUser.date = user.date;
+            newUser.country = user.country;
+            newUser.phoneNumber = user.phoneNumber;
+            newUser.password = user.password;
+            return participantRegistry.add(newUser);
+          });
+      } else {
+        throw new Error("Este correo ya esta registrado");
+      }
+    })
+    .catch(function (error) {
+      throw new Error(error);
+    });
 }
 
 /**
- * Creacion de owner y receiver transaction processor function.
- * @param {org.transfer.tfg.login} login The login transaction instance.
- * @transaction
- */
+* Creacion de owner y receiver transaction processor function.
+* @param {org.transfer.tfg.login} login The login transaction instance.
+* @transaction
+*/
 function login(user) {
-  		var password= user.password;
-  		var userId= user.user.email
-		return getParticipantRegistry('org.transfer.tfg.User')
-            .then(function (participantRegistry) {
-                 return participantRegistry.get(userId);
-            })
-            .then(function (userAux) {
-              if(password !== userAux.password){
-                throw new Error("La contraseña no es correcta");
-              }
-            })
-        .catch(function (error) {
-          throw new Error(error);
-        });
+  var password = user.password;
+  var userId = user.user.email
+
+  return getParticipantRegistry('org.transfer.tfg.User')
+    .then(function (participantRegistry) {
+      return participantRegistry.exists(userId);
+    })
+    .then(function (exists) {
+      if (exists) {
+        return getParticipantRegistry('org.transfer.tfg.User')
+          .then(function (participantRegistry) {
+            return participantRegistry.get(userId);
+          })
+          .then(function (userAux) {
+            if (password !== userAux.password) {
+              throw new Error("La contraseña no es correcta");
+            }
+          });
+      } else {
+        throw new Error("El usuario no existe");
+      }
+    })
+    .catch(function (error) {
+      throw new Error(error);
+    });
 }
